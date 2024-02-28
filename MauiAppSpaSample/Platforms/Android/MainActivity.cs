8a5303dd5dbe8941c9ccf014f45d9db40c515d98
Trudy <trudy@vpnhood.com>
2024-02-28 00:13:14 -0800

    android spa

--- MauiAppSpaSample/Platforms/Android/MainActivity.cs
+++ MauiAppSpaSample/Platforms/Android/MainActivity.cs
@@ -1,10 +1,46 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
-using Android.OS;
+using Android.Service.QuickSettings;
+using Android.Views;
+using VpnHood.Client.App.Droid.Common.Activities;
+using VpnHood.Client.App.Droid.GooglePlay;
 
 namespace VpnHood.Client.Samples.MauiAppSpaSample;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
+[Activity(
+    Label = "@string/app_name",
+    Theme = "@android:style/Theme.DeviceDefault.NoActionBar",
+    MainLauncher = true,
+    Exported = true,
+    WindowSoftInputMode = SoftInput.AdjustResize, // resize app when keyboard is shown
+    AlwaysRetainTaskState = true,
+    LaunchMode = LaunchMode.SingleInstance,
+    ScreenOrientation = ScreenOrientation.Unspecified,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.LayoutDirection |
+                           ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.FontScale |
+                           ConfigChanges.Locale | ConfigChanges.Navigation | ConfigChanges.UiMode)]
+
+[IntentFilter([TileService.ActionQsTilePreferences])]
+[IntentFilter([Intent.ActionMain], Categories = [Intent.CategoryLauncher, Intent.CategoryLeanbackLauncher])]
+[IntentFilter([Intent.ActionView], Categories = [Intent.CategoryDefault], DataScheme = "content", DataMimeTypes = [AccessKeyMime1, AccessKeyMime2, AccessKeyMime3])]
+[IntentFilter([Intent.ActionView], Categories = [Intent.CategoryDefault, Intent.CategoryBrowsable], DataSchemes = [AccessKeyScheme1, AccessKeyScheme2])]
+public class MainActivity : MauiAppMainActivity
 {
+    // https://android.googlesource.com/platform/libcore/+/android-5.0.2_r1/luni/src/main/java/libcore/net/MimeUtils.java
+    public const string AccessKeyScheme1 = "vh";
+    public const string AccessKeyScheme2 = "vhkey";
+    public const string AccessKeyMime1 = "application/vhkey";
+    public const string AccessKeyMime2 = "application/pgp-keys"; //.key
+    public const string AccessKeyMime3 = "application/vnd.cinderella"; //.cdy
+
+    protected override AndroidAppMainActivityHandler CreateMainActivityHandler()
+    {
+        return new AndroidAppWebViewMainActivityHandler(this, new AndroidMainActivityWebViewOptions
+        {
+            AccessKeySchemes = [AccessKeyScheme1, AccessKeyScheme2],
+            AccessKeyMimes = [AccessKeyMime1, AccessKeyMime2, AccessKeyMime3],
+            AppUpdaterService = new GooglePlayAppUpdaterService(this)
+        });
+    }
 }
