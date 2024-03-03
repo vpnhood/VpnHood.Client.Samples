1913aa3e65fa4f40888526190b0f271e9a80971a
Trudy <trudy@vpnhood.com>
2024-03-02 21:46:58 -0800

    Publish

--- UpdateAndPush.ps1
+++ UpdateAndPush.ps1
@@ -8,6 +8,9 @@ try
 	# Update nugets
 	dotnet-outdated -u;
 
+	# build
+	dotnet build
+
 	# commit and push
 	git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish";
 	git --git-dir=$gitDir --work-tree=$solutionDir pull;
