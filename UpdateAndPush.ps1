17f7af989b56dc753a0fabbed821cce418d483a7
Trudy <trudy@vpnhood.com>
2024-03-02 21:40:00 -0800

    add update and push script

---
+++ UpdateAndPush.ps1
@@ -0,0 +1,20 @@
+# update
+$solutionDir = $PSScriptRoot;
+$gitDir = "$solutionDir/.git";
+try
+{
+	Push-Location -Path "$solutionDir";
+	
+	# Update nugets
+	dotnet-outdated -u;
+
+	# commit and push
+	git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish";
+	git --git-dir=$gitDir --work-tree=$solutionDir pull;
+	git --git-dir=$gitDir --work-tree=$solutionDir push;
+}
+finally
+{
+	Pop-Location
+}
+
