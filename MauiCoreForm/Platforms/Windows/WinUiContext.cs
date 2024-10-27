1c31463781b063a455ebc8755e8ccb90b14b2fe1
Trudy <trudy@vpnhood.com>
2024-10-26 21:12:11 -0700

    fix WinUiContext for sample

---
+++ MauiCoreForm/Platforms/Windows/WinUiContext.cs
@@ -0,0 +1,8 @@
+﻿using VpnHood.Client.Device;
+
+namespace VpnHood.Client.Samples.MauiCoreForm.WinUI;
+
+public class WinUiContext : IUiContext
+{
+    public bool IsActive => true; //always true in sample
+}
